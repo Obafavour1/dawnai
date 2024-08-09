eba225f2a6e48541f16ac2e72846bafd5063e54a
Tonichris01 <gemsbatinterns@gmail.com>
2024-08-09 16:03:52 +0100

    another

--- components/mobiletopbar.tsx
+++ components/mobiletopbar.tsx
@@ -13,12 +13,12 @@ const MobileTopbar = () => {
     <button onClick={()=>isOpen(!open)} className="flex align-center gap-3">
         <Image src="/menu.png" alt="Back" width={30} height={30}/>
           <div className={`${open ? "visible" : "invisible"}`}>
-          <div className="absolute top-0 right-0 bg-black md:flex md:flex-col h-[895px]  pt-3  w-[300px] items-center ml-[6px] mt-[35px]  text-[#ffffff] border-solid border-2 rounded-xl border-white">
+          <div className="absolute px-3 -top-8 right-0 bg-black md:flex md:flex-col h-screen  pt-3  w-[300px] items-center ml-[6px] mt-[35px]  text-[#ffffff] border-solid border-2 rounded-xl border-white">
             <div>
               <Image src="/logo.png" alt="DAWN LOGO" width={158} height={33} className="w-[120px]" />
             </div>
 
-            <div className="flex flex-col py-2.5 items-start  gap-4 mt-24">
+            <div className="flex flex-col py-2.5 items-start  gap-4 mt-5">
               {navItems.map((nav) =>(
                 < Link key={nav.id} href={nav.href} className="flex items-center lg:gap-3.5 py-2 px-4  rounded-[15px]  hover:bg-gradient-to-r from-[#9A4FFF] to-[#A124B6]">
                   <div>
@@ -29,7 +29,7 @@ const MobileTopbar = () => {
               ))}
             </div>
 
-            <div className="mt-16 flex flex-col gap-2 items-center">
+            <div className="mt-5 flex flex-col gap-2 items-center">
               <Image src='/ai.png' alt='AI Image' width={205} height={136} />
               <p>Dawn Teachers</p>
             </div>
